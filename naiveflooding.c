adb302d1bcef8f6a24c7435d1b52da26c6170350
Andrei Brabete <brabete.andrei96@gmail.com>
2018-02-12 02:05:21 +0100

    Got log for trickle and changed naive flooding.

--- naiveflooding.c
+++ naiveflooding.c
@@ -7,6 +7,7 @@
 
 #include "contiki.h"
 #include "net/rime/rime.h"
+#include "net/rime/trickle.h"
 #include "random.h"
 
 #include "dev/leds.h"
@@ -24,13 +25,14 @@ struct message{
   uint16_t flood_id;
   uint8_t node_count;
   uint8_t nodes[NO_OF_NODES];
+  uint8_t from;
 };
 
 struct message payload_send,payload_recv;
-static struct broadcast_conn broadcast;
+static struct trickle_conn trickle;
 static struct ctimer message_sent;
 /*---------------------------------------------------------------------------*/
-PROCESS(main_process, "Naive Flooding");
+PROCESS(main_process, "Trickle Flooding (IMPROVEMENT)");
 AUTOSTART_PROCESSES(&main_process);
 
 /* callback function to request a message 
@@ -42,17 +44,16 @@ request_message(void *ptr){
   printf("Send me a message\n");
 }
 
-#define PROBABILITY 0.2
 
 /*---------------------------------------------------------------------------*/
 static void
-broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from)
+trickle_recv(struct trickle_conn *c)
 {
   bool received = false;
 
   packetbuf_copyto(&payload_recv);
 
-  printf("Broadcast recv from %d of %d with id %d\n", from->u8[0],payload_recv.nodes[0],payload_recv.flood_id);
+  printf("Broadcast recv from %d of %d with id %d\n", payload_recv.from, payload_recv.nodes[0],payload_recv.flood_id);
   
   int i = 0;
   for(i = 0; i < payload_recv.node_count;i++){
@@ -69,29 +70,29 @@ broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from)
   if(!received){
     payload_recv.nodes[payload_recv.node_count]  = linkaddr_node_addr.u8[0];
     payload_recv.node_count = payload_recv.node_count + 1;
-
-    if(((double)rand() / (double)RAND_MAX) <= PROBABILITY) {	
-    	packetbuf_copyfrom(&payload_recv,sizeof(payload_recv));
-    	broadcast_send(&broadcast);
-    	printf("Broadcast message sent\n");
-    }	
+    payload_recv.from = linkaddr_node_addr.u8[0];
+   
+    packetbuf_copyfrom(&payload_recv,sizeof(payload_recv));
+    trickle_send(&trickle);	
+    printf("Broadcast message sent\n");
+    
     if(linkaddr_node_addr.u8[0] == 1){
       ctimer_reset(&message_sent);
     }
   }
 }
 
-static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
+static const struct trickle_callbacks trickle_call = {trickle_recv};
 
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(main_process, ev, data)
 {
-  PROCESS_EXITHANDLER(broadcast_close(&broadcast);)
+  PROCESS_EXITHANDLER(trickle_close(&trickle);)
   char *buf;
 
   PROCESS_BEGIN();
   //powertrace_start(CLOCK_SECOND * 10);
-  broadcast_open(&broadcast, 129, &broadcast_call);
+  trickle_open(&trickle, CLOCK_SECOND, 129, &trickle_call);
   if(linkaddr_node_addr.u8[0] == 1){
     ctimer_set(&message_sent, CLOCK_SECOND * 10,request_message,NULL);   
   }
@@ -104,8 +105,9 @@ PROCESS_THREAD(main_process, ev, data)
       payload_send.flood_id = atoi(buf);
       payload_send.nodes[0] = linkaddr_node_addr.u8[0];
       payload_send.node_count = 1;
+      payload_recv.from = linkaddr_node_addr.u8[0];
       packetbuf_copyfrom(&payload_send, sizeof(payload_recv));
-      broadcast_send(&broadcast);
+      trickle_send(&trickle);
       printf("Broadcast message sent %d\n",atoi(buf));
 
       if(linkaddr_node_addr.u8[0] == 1){
